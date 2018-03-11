bcfad37339b4a27b9d85cb7914ba423ed7d363d5
Christilyn Arjona <christilynarjona@outlook.com>
2018-03-11 14:48:56 -0600

    Update Tree.hs

--- Tree.hs
+++ Tree.hs
@@ -3,3 +3,32 @@ data Node = Node { parent :: Node,
 					task :: Char,
 					children :: [Node]
 					} deriving (Show)
+					
+-- Take out the tasks that are already taken from the availableTasks array
+-- params: availableTasks, takenTasks
+-- return: takenTasks updated
+getAvailTasks :: [Char] -> [Char] -> [Char]
+getAvailTasks a [] = a
+getAvailTasks a (y:ys) = getAvailTasks (secondLoop a y) ys
+
+secondLoop ::[Char] -> Char -> [Char]
+secondLoop [] _ = []
+secondLoop (x:xs) y = thirdLoop x y ++ secondLoop xs y 
+
+thirdLoop :: Char -> Char -> [Char]
+thirdLoop x y   | x == y = []
+                | otherwise = [x]
+---------------------------------------------------------------------------
+
+----------------------------- create children -----------------------------
+-- params: parentNode, availableTasks, takenTasks
+createChildren ::  Int -> Node -> [Char] -> [Char] -> [Node]
+createChildren _ _ [] [] = []
+createChildren pn at tt = if (machine pn) == -1 
+                            then do
+								 
+								 
+			  else if (machine pn) == 6
+			     then do
+							
+			  else do
