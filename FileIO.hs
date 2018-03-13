98d1b3013c77fad99431b90925bab877733aa578
DESKTOP-3T992KK\Esther <hhakung@gmail.com>
2018-03-13 11:51:37 -0600

    added data types

--- FileIO.hs
+++ FileIO.hs
@@ -1,7 +1,23 @@
 import System.IO
 import System.Environment
 
+forcedPartial = [(Int, Char)]
+
+data listofTuples_IntChar = [(Int, Char)]
+data listofTuples_CharChar = [(Char, Char)]
+data listof8Ints = [(Int, Int, Int, Int, Int, Int, Int, Int)]
+data listofTriples_CharCharInt = [(Char, Char, Int)]
+
 main = do
     (inputFile:outputFile:_) <- getArgs
     contents <- readFile inputFile
     putStrLn contents
+	
+	forcedPartial::listofTuples_IntChar
+	forbideenMachine::listofTuples_IntChar
+	tooNearTasks::listofTuples_CharChar
+	machinePenalties::listof8Ints
+	tooNearPenalties::listofTriples_CharCharInt
+	
+-- function to parse the contents into data types
+parseContents :: Char[] -> listofTuples_IntChar -> listofTuples_IntChar -> listofTuples_CharChar -> listof8Ints -> listofTriples_CharCharInt
