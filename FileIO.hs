9fc706b5762033c5b707ae891a2af3850193ef98
kimgahyung <kimgahyung@gmail.com>
2018-03-14 13:29:15 -0600

    Comment: Added the function that finds the index and split the array

--- FileIO.hs
+++ FileIO.hs
@@ -1,7 +1,20 @@
 import System.IO
 import System.Environment
+import Data.List
+import Data.Maybe
 
 forcedPartial = [(Int, Char)]
+
+-- Returns the index of string
+getIndex :: [String] -> String -> [Maybe Int]
+getIndex lst str = do
+    return (elemIndex str lst)
+
+-- Returns the parsed list  
+splitList :: [String] -> Int -> [([String],[String])]
+splitList lst ind = do
+    let parsedList = splitAt ind lst
+    return parsedList
 
 data listofTuples_IntChar = [(Int, Char)]
 data listofTuples_CharChar = [(Char, Char)]
@@ -11,7 +24,17 @@ data listofTriples_CharCharInt = [(Char, Char, Int)]
 main = do
     (inputFile:outputFile:_) <- getArgs
     contents <- readFile inputFile
-    putStrLn contents
+    let linesOfFile = lines contents 
+    
+    -- Find the indices of strings 
+    let forcedIndex = fromJust $ head $ getIndex linesOfFile "forced partial assignment:"
+    let forbiddenIndex = fromJust $ head $ getIndex linesOfFile "forbidden machine:"
+    let hardTooNearIndex = fromJust $ head $ getIndex linesOfFile "too-near tasks:"
+    let machinePenaltiesIndex = fromJust $ head $ getIndex linesOfFile "machine penalties:"
+    let softTooNearIndex = fromJust $ head $ getIndex linesOfFile "too-near penalities"
+    
+    
+    
 	
 	forcedPartial::listofTuples_IntChar
 	forbideenMachine::listofTuples_IntChar
