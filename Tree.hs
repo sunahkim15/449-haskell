4a880eaa176125070d08342d64b209b47908776a
Esther Chung <hhakung@gmail.com>
2018-03-16 04:40:51 -0600

    Edited the rose tree structure and added a function to get the parent node

--- Tree.hs
+++ Tree.hs
@@ -1,9 +1,29 @@
-data Node = Node { parent :: Node,
-                                        machine :: Int,
-                                        task :: Char,
-                                        children :: [Node]
-                                        } deriving (Show)
-					
+-- note: DON'T KNOW IF THIS WORKS OR NOT; HAVEN'T TESTED
+
+-- a and b are type classes; for our case, a should be Int and b should be Char
+-- For example, you can create a RoseTree this way:
+-- ourTree = Node { machine = 1, task = A, children = [ Node { machine = 2, task = B, children = [] },
+--                                                      Node { machine = 2, task = C, children = [ Node { machine = 3, task = D, children = []] } ]
+
+data RoseTree a b = Node { machine :: a,
+                           task :: b,
+						   children :: [RoseTree a b] }
+						   deriving Show
+						   
+data onePath a b = Child a b [RoseTree a b]
+			   
+data Paths a b = [onePath a b]
+
+---------------------------- get the parent node ----------------------------
+-- Get (create) the parent node;
+-- note: he list of the children in the parent node that gets created is not in order;
+-- For example, the children list may be like this: 
+-- children = [sub tree for machine 2 task 1, sub tree for machine 2 task 4, sub tree for machine 2 task 2, ...]
+-- params: The current subtree of the rose tree, the path
+getParentNode :: (RoseTree a b, Paths a b) -> (RoseTree a b, Paths a b)
+getParentNode (t, Child a b [ts]:bs) = (Node a b t:ts, bs)
+
+--------------------------- get available tasks ------------------------------
 -- Take out the tasks that are already taken from the availableTasks array
 -- params: availableTasks, takenTasks
 -- return: availableTasks updated
@@ -11,15 +31,17 @@ getAvailTasks :: [Char] -> [Char] -> [Char]
 getAvailTasks a [] = a
 getAvailTasks a (y:ys) = getAvailTasks (secondLoop a y) ys
 
+--------------------------- second loop --------------------------------------
 secondLoop ::[Char] -> Char -> [Char]
 secondLoop [] _ = []
 secondLoop (x:xs) y = thirdLoop x y ++ secondLoop xs y 
 
+--------------------------- third loop ---------------------------------------
 thirdLoop :: Char -> Char -> [Char]
 thirdLoop x y   | x == y = []
                 | otherwise = [x]
 
----------------------------- create child loop ----------------------------
+---------------------------- create child loop -------------------------------
 -- params: counter, parentNode, penaltyArray, availableTasks
 createChildLoop :: Int -> Node -> [Char] -> [Node]
 createChildLoop i pn pa at = if i>= (length at) then []
