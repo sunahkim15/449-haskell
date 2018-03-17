362ab8cdf81d6dbb0fcd094bf24fa088e8f948f7
jmgrageda <35971193+jmgrageda@users.noreply.github.com>
2018-03-17 16:11:36 -0600

    Update TNH.hs

--- TNH.hs
+++ TNH.hs
@@ -2,6 +2,7 @@
 --takes in list of available tasks, the 2d pa for too near hard, and an assignment
 tooNearHard :: [Int] -> [[Bool]] -> Assignment -> [(Int,Bool)]
 tooNearHard at pa a = result
-    where task1 = task a
-          taskList = availTask
-          result = [(x, ((pa !! task1) !! x))| x <- taskList)]
+  where task1 = task a
+        parentTask = charToInt task1
+        taskList = availTask
+        result = [(x, ((pa !! parentTask) !! x))| x <- taskList]
