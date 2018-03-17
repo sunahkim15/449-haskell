c3f1e29c61372651045f4686c4ca668e56128924
sunahkim15 <sunah.kim@ucalgary.ca>
2018-03-17 14:42:32 -0600

    no message

--- functions.hs
+++ functions.hs
@@ -34,28 +34,21 @@ minP [] = error "empty list"
 minP [x] = x  
 minP (x:y:xs) = if (x == -1) then minP(y:xs) else if (y == -1) then minP(x:xs) else if (x < y) then minP(x:xs) else minP(y:xs)
 
---dont need 
---checkForbidden :: Int -> Int -> [[Int]] -> Bool
---checkForbidden m t fa = if ((fa !! m) !! t) == (-1) then True else False
+--filterFalse :: [(Int, Bool)] -> [Maybe (Int, Bool)]
+--filterFalse [(t, b)] = if b == True then [Nothing] else [(t, b)]
+--filterFalse [(t, b):xs] = if b == True then filterFalse [xs] else filterFalse
+filterNot :: (a -> Bool) -> [a] -> [a]
+filterNot pred = filter $ not . pred 
 
---map :: (Int -> Int -> [[Int]] -> Bool) -> [a] -> [b]  
---map _ [] = []  
---map f (x:xs) = f x : map f xs
+data Assignment = Assignment {machine :: Int, task :: Char, penalty :: Int} deriving (Show)
 
---list of whats been assigned
---list of tasks available 
---list of children
-    --list of lowerbound from list of children using map
-    --use to get child with lowest lowerbound  
-
-data Assignment = Assignment {machine :: Int, task :: Char} deriving (Show)
 main = do
 let penaltyarray = [[10,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]] 
 let machinetemp = 0
 --we need to find task for machine 0
 --get best task for machine 0
 let tasktemp = getTaskFromPenalty machinetemp (lowestMTpenalty machinetemp penaltyarray) penaltyarray
-let m0 = Assignment machinetemp tasktemp
+let m0 = Assignment machinetemp tasktemp ((penaltyarray !! machinetemp) !! charToInt(tasktemp))
 let history = [m0] 
 let temp = machinetemp
 let machinetemp = succ temp
--- initialsol.hs
+++ initialsol.hs
@@ -1,36 +1,29 @@
 
 data Assignment = Assignment {machine :: Int, task :: Char, penalty :: Int} deriving (Show)
 
-main = do
-let penaltyarray = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]] 
-let machinetemp = 0
---we need to find task for machine 0
---get best task for machine 0
-let tasktemp = machinetemp (bestMachineMatch machinetemp penaltyarray) penaltyarray
-let m0 = Assignment machinetemp tasktemp
-print m0
-
 initsol :: Int -> [Assignment] -> [[Int]] -> [[Bool]] -> [[Int]] -> [Assignment]
 initsol 6 h pa th ts = 
 --available task (task to int)
 let machinetemp = 6
-let allTask = [0,1,2,3,4,5,6,7]
-let availTask = filter not(`elem`(map chartoInt (map task h))) allTask
+    allTask = [0,1,2,3,4,5,6,7]
+    availTask = filter not(`elem`(map chartoInt (map task h))) allTask
 --function find best task for machine 6 
-    --calc penalties previous
-let m = map machine h
-let t = map task h
---note*****might be good to put penalty in assignment
-	--get row of penalty for machine 6 from pa
-let pr = pa !! machinetemp
-	--for each penalty task for machine 6 
-	    --check too near hard constraint for task (task for 6,7 and 7,0)
-		--if false add any too near soft constraint to penalty of task
-	--use minP to get task for machine 6
-	--create machine 6 Assignment and put in history
-	--machinetemp ++
-	--call initsol machinetemp h pa th ts 
+    --calc penalties previous (penalties previous includes toonear penalties)
+    prePenalties = sum (map penalty h)
+    m = map machine h
+    t = map task h
+    --for each penalty task for machine 6 
+        --check too near hard constraint for task (task for 6,7 and 7,0)
+    listTNHfalse = filterNot snd listTNH 
+        --if false add any too near soft constraint to penalty of task
+    penaltyRow = pa !! machinetemp
+    penaltyRowAdded = [if x == fst y then (penaltyRow !! x) + snd y else (penaltyRow !! x)| x <- 0 .. 7, y <- listTNS]
+    --use minP to get task for machine 6
+    --create machine 6 Assignment and put in history
+    --machinetemp ++
+    --call initsol machinetemp h pa th ts 
 initsol 7 h pa th ts = 
+let machinetemp = 7
 --same as above but at end return history 
 initsol m h pa th ts = 
 --same as 6 but check one too near hard/soft constraint
