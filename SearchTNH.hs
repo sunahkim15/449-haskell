3f6c8c7943afee06ad22aa8ea1dee2e44a0c15f4
jmgrageda <35971193+jmgrageda@users.noreply.github.com>
2018-03-18 16:56:17 -0600

    Update SearchTNH.hs

--- SearchTNH.hs
+++ SearchTNH.hs
@@ -1,7 +1,7 @@
 tooNearHard :: Char -> [Char]
 tooNearHard parentTask = listOfChars
   where parent = charToInt parentTask
-        result = [x | x <- (0..7), ((penalties !! parent) !! x) == True] 
+        result = [x | x <- [0..7], ((penalties !! parent) !! x) == True] 
         listOfChars = [intToChar c | c <- result]
         
         
